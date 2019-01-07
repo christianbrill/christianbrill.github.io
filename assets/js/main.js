8a2f4449f4ff67b415662fa031de22c49d7ffb3f
Christian Brill <quedarme@gmail.com>
2019-01-07 12:20:06 +0100

    includes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,27 @@
-// let logoArray = ['logo-orange.svg', 'logo-yellow.svg', 'logo-green.svg', 'logo-blue.svg', 'logo-purple.svg', 'logo-pink.svg', 'logo-lightblue.svg', 'logo-brown.svg', 'logo-black.svg'];
-//
-// function setHeroImage() {
-//     $.each(logoArray, function (index, value) {
-//         $(".hero img").attr('src', 'assets/img/' + value);
-//     });
-// }
-//
-// setHeroImage();
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};

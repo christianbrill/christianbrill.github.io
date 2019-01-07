1bb7a87a32d7dfbf8d845bad0a8aa7e560665cab
Christian Brill <quedarme@gmail.com>
2019-01-07 12:09:36 +0100

    hero change

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// let logoArray = ['logo-orange.svg', 'logo-yellow.svg', 'logo-green.svg', 'logo-blue.svg', 'logo-purple.svg', 'logo-pink.svg', 'logo-lightblue.svg', 'logo-brown.svg', 'logo-black.svg'];
+//
+// function setHeroImage() {
+//     $.each(logoArray, function (index, value) {
+//         $(".hero img").attr('src', 'assets/img/' + value);
+//     });
+// }
+//
+// setHeroImage();
